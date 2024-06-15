34dbffd541ba2d3ada8a74d41cefb6714ae3bd3c
Ahmansavthor Batista <ahmansavthor.batista@ithealth.co>
2024-06-15 12:40:55 -0500

    Ci: Estructurar proyecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+    },
+    {
+        path: 'auth',
+    },
+    {
+        path: 'admin',
+    },
+    {
+        path: '**',
+        redirectTo: '',
+        pathMatch: 'full'
+    }
+];
