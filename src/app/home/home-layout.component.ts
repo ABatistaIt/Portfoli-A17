a2f5bffa8f7c120982d9d52a9dc82659dc86eddf
Ahmansavthor Batista C <122812772+ABatistaIt@users.noreply.github.com>
2024-09-01 11:19:09 -0500

    Ci: Init

--- src/app/home/home-layout.component.ts
+++ src/app/home/home-layout.component.ts
@@ -1,24 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { HeaderMainComponent } from '../shared/components/header-main/header-main.component';
+import { FooterMainComponent } from '../shared/components/footer-main/footer-main.component';
 
 @Component({
   selector: 'app-home-layout',
   standalone: true,
-  imports: [RouterLink, RouterOutlet],
+  imports: [RouterLink, RouterOutlet, HeaderMainComponent, FooterMainComponent],
   template: `
-    <header>
-      <h1>HOME - HEADER</h1>
-    </header>
-    <nav>
-      <ul>
-        <li><a routerLink="/admin">Admin</a></li>
-        <li><a routerLink="/auth">Auth</a></li>
-      </ul>
-    </nav>
-    <router-outlet></router-outlet>
-    <footer>
-      <h3>HOME -- FOOTER</h3>
-    </footer>
+    <!-- Header Main -->
+    <app-header-main></app-header-main>
+    
+    <!-- Enrutamiento principal -->
+    <main>
+      <router-outlet></router-outlet>
+    </main>
+    
+    <!-- Footer Main -->
+     <app-footer-main></app-footer-main>
   `,
   styleUrl: './home-layout.component.scss'
 })
